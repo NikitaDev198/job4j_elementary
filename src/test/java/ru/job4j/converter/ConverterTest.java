98a948b7f96061664e51e23da6e72a30cd7d6991
Nikita <nikitadevj178@gmail.com>
2023-09-29 23:05:45 +0300

    Добавление теста ConverterTest.java на github [243579]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,26 @@
+package ru.job4j.converter;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.*;
+
+class ConverterTest {
+
+    @Test
+    void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+
+    }
+
+    @Test
+    void whenConvert240RblThen4Dollars() {
+        float in = 240;
+        float expected = 4;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        assertThat(out).isEqualTo(expected, withPrecision(eps));
+    }
+}
