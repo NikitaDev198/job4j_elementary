65c103a65386eccbe248f16758f1bc2be9698cda
Nikita <nikitadevj178@gmail.com>
2023-10-02 23:16:56 +0300

    Добавление файла Board.java на github [243616]

---
+++ src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,26 @@
+package ru.job4j.loop;
+
+public class Board {
+
+    public static void paint(int width, int height) {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                int rsl = i + j;
+                if (rsl % 2 == 0) {
+                    System.out.print("X");
+                } else {
+                    System.out.print(" ");
+                }
+
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        paint(3, 3);
+        System.out.println();
+        paint(4, 4);
+    }
+
+}
