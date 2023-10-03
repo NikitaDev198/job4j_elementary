28cff6bcc399c73f8dc15f54e07c37004785f5ba
Nikita <nikitadevj178@gmail.com>
2023-10-03 21:22:28 +0300

    Исправление файла DivideBySix.java на github [243618]

--- src/main/java/ru/job4j/condition/DivideBySix.java
+++ src/main/java/ru/job4j/condition/DivideBySix.java
@@ -3,17 +3,14 @@ package ru.job4j.condition;
 public class DivideBySix {
 
     public static String checkNumber(int number) {
-        String rsl = "";
+        String rsl;
         if (number % 2 == 0 && number % 3 == 0) {
             rsl = "The number divides by 6.";
-        }
-        if (number % 2 != 0 && number % 3 == 0) {
+        } else if (number % 3 == 0) {
             rsl = "The number divides by 3, but it isn't the even number.";
-        }
-        if (number % 2 == 0 && number % 3 != 0) {
+        } else if (number % 2 == 0) {
             rsl = "The number doesn't divide by 3, but it is the even number.";
-        }
-        if (number % 2 != 0 && number % 3 != 0) {
+        } else {
             rsl = "The number doesn't divide by 3 and it isn't the even number.";
         }
         return rsl;
