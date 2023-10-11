02e8ecf39d6479aea6b1d7eeab2fc2d5125542be
Nikita <nikitadevj178@gmail.com>
2023-10-11 23:02:49 +0300

    Добавление файлов: SimpleStringEncoder.java, SimpleStringEncoderTest.java на github [243672]

---
+++ src/main/java/ru/job4j/array/SimpleStringEncoder.java
@@ -0,0 +1,24 @@
+package ru.job4j.array;
+
+public class SimpleStringEncoder {
+
+    public static String encode(String input) {
+        String result = "";
+        char symbol = input.charAt(0);
+        int counter = 1;
+
+        for (int i = 1; i <= input.length() - 1; i++) {
+            if (symbol == input.charAt(i)) {
+                counter++;
+            } else {
+                result += Character.toString(symbol) + (counter > 1 ? counter : "");
+                counter = 1;
+            }
+            symbol = input.charAt(i);
+            result += i == input.length() - 1 ? Character.toString(symbol) + (counter > 1 ? counter : "") : "";
+        }
+
+        return result;
+    }
+
+}
