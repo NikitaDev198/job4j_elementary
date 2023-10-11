50b50deb3a756b8ae20e9178fbb652217a8eb462
Nikita <nikitadevj178@gmail.com>
2023-10-11 23:35:44 +0300

    Исправление файла TwoNumberSum.java на github [243673]

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -3,20 +3,16 @@ package ru.job4j.array;
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
         int i = 0;
-        int j = 1;
-        while (i < array.length) {
+        int j = array.length - 1;
+        while (i < j) {
             if (array[i] + array[j] == target) {
                 return SortSelected.sort(new int[]{j, i});
             }
-            if (i + 1 == array.length) {
-                j++;
-                i = j + 1;
-            }
-            if (i + 1 == j) {
-                i += 2;
+            if (array[i] + array[j] < target) {
+                i++;
             } else {
-                    i++;
-                }
+                j--;
+            }
         }
         return new int[0];
     }
