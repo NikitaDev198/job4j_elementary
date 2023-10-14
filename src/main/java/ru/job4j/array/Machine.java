c8073997b09edbab2d5fe893435fac1875116a6c
Nikita <nikitadevj178@gmail.com>
2023-10-14 15:31:27 +0300

    Исправление файла Machine.java на github [243676]

--- src/main/java/ru/job4j/array/Machine.java
+++ src/main/java/ru/job4j/array/Machine.java
@@ -8,10 +8,10 @@ public class Machine {
         int[] coins = {10, 5, 2, 1};
         int[] rsl = new int[100];
         int size = 0;
-        for (int i : coins) {
-            while (money - i >= price) {
-                money -= i;
-                rsl[size] = i;
+        for (int coin : coins) {
+            while (money - coin >= price) {
+                money -= coin;
+                rsl[size] = coin;
                 size++;
             }
         }
