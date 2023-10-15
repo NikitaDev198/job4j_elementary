504585ce5eb79a4f8d7c4dc85e31ea307948c5e6
Nikita <nikitadevj178@gmail.com>
2023-10-15 17:37:28 +0300

    Изменение файлов: Max.java, MaxTest.java на github [243679]

--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -32,4 +32,25 @@ class MaxTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @Test
+    void whenFirst5ToSecond2ToThird31Then31() {
+        int first = 5;
+        int second = 2;
+        int third = 31;
+        int result = Max.max(first, second, third);
+        int expected = 31;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst3ToSecond18ToThird10ToFourth12Then18() {
+        int first = 3;
+        int second = 18;
+        int third = 10;
+        int fourth = 12;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 18;
+        assertThat(result).isEqualTo(expected);
+    }
+
 }
