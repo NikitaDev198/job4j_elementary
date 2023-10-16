62ade590ee7ee5f846d0ce779aa5f07fdc536479
Nikita <nikitadevj178@gmail.com>
2023-10-16 17:02:00 +0300

    Исправление файла Max.java на github [243680]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -3,11 +3,11 @@ package ru.job4j.condition;
 public class Max {
 
     public static int max(int first, int second, int third) {
-        return first > max(second, third) ? first : max(second, third);
+        return max(first, max(second, third));
     }
 
     public static int max(int first, int second, int third, int fourth) {
-        return first > max(second, third, fourth) ? first : max(second, third, fourth);
+        return max(first, max(second, third, fourth));
     }
 
     public static int max(int left, int right) {
